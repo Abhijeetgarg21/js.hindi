ebdef95e929d52b73532dc424b13a329b44f1cce
Abhijeetgarg21 <91984904+Abhijeetgarg21@users.noreply.github.com>
2025-11-01 15:16:26 +0000

    conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(3 
+    + 3)  // code readability should be high
+console.log("abhijeet")
+
+let name = "abhijeet"
+let age = 18
+let isLoggedIn = false 
+let state;
+
+
+// nuumber => 2 to the power  53
+// bigint
+// string  => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "abhijeet"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
